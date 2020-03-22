b1469e984768a80b64db2c4c6304ccde788e60c3
Matthew Dillon <matthewrdillon@gmail.com>
2020-03-22 16:57:06 -0700

    SQUASH

--- bin/generate_vscode_projects.py
+++ bin/generate_vscode_projects.py
@@ -1,21 +1,30 @@
 import json
 import os
 
-from projects import PROJECTS
+from qiime2_projects import PROJECTS as Q2_PROJECTS
 
 
-folders = []
-for org, repos in PROJECTS.items():
-    for repo in repos:
-        folders.append({
-            'name': repo.lower(),
-            'path': os.path.join(os.sep, 'Users', 'matthew', 'src', org, repo)
-        })
+def render_vscode_workspace(projects, output_fp, include_dotfiles=False):
+    folders = []
+    for org, repos in projects.items():
+        for repo in repos:
+            folders.append({
+                'name': repo.lower(),
+                'path': os.path.join(os.sep, 'Users', 'matthew', 'src', org, repo)
+            })
+
+    if include_dotfiles:
+        dotfile_fp = os.path.join(os.sep, 'Users', 'matthew', '.dotfiles')
+        folders.append({'name': 'dotfiles',
+                        'path': dotfile_fp})
+
+    with open(output_fp, 'w') as fh:
+        json.dump({'folders': sorted(folders, key=lambda x: x['name'])}, fh, sort_keys=True, indent=4)
 
 
-folders.append({'name': 'dotfiles',
-                'path': os.path.join(os.sep, 'Users', 'matthew', '.dotfiles')})
-
-
-with open('vscode/qiime2.code-workspace', 'w') as fh:
-    json.dump({'folders': sorted(folders, key=lambda x: x['name'])}, fh, sort_keys=True, indent=4)
+if __name__ == '__main__':
+    render_vscode_workspace(
+        Q2_PROJECTS,
+        os.path.join('vscode', 'qiime2.code-workspace'),
+        include_dotfiles=True,
+    )
