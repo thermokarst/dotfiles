6559f3cbede10b16c03de1277ef5f5bd6a5020ce
Matthew Dillon <matthewrdillon@gmail.com>
2020-03-25 20:59:52 -0700

    SQUASH

--- bin/generate_vscode_workspaces.py
+++ bin/generate_vscode_workspaces.py
@@ -6,21 +6,26 @@ from mds_projects import PROJECTS as MDS_PROJECTS
 
 
 def render_vscode_workspace(projects, project_name, output_fp,
-                            include_dotfiles=True):
+                            include_dotfiles=True, extra_dirs=None):
     folders = []
     for org, repos in projects.items():
         for repo in repos:
             fp = os.path.join('~', 'projects', project_name, repo)
             fp = os.path.expanduser(fp)
-            folders.append({
-                'name': repo.lower(),
-                'path': fp,
-            })
+            folders.append({'name': repo.lower(),
+                            'path': fp})
 
     if include_dotfiles:
         dotfile_fp = os.path.join(os.sep, 'Users', 'matthew', '.dotfiles')
         folders.append({'name': 'dotfiles',
                         'path': dotfile_fp})
+
+    if extra_dirs is not None:
+        for name, path in extra_dirs.items():
+            fp = os.path.join('~', 'projects', project_name, path)
+            fp = os.path.expanduser(fp)
+            folders.append({'name': name,
+                            'path': fp})
 
     with open(output_fp, 'w') as fh:
         json.dump({'folders': sorted(folders, key=lambda x: x['name'])},
@@ -32,6 +37,7 @@ if __name__ == '__main__':
         Q2_PROJECTS,
         'qiime2',
         os.path.join('vscode', 'qiime2.code-workspace'),
+        extra_dirs={'data': 'data'},
     )
 
     render_vscode_workspace(
